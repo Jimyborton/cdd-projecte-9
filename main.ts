bc6692133b9db5888e4c74be1e84ebcf2939a365
José García de las Bayonas Martinez <86870579+Jimyborton@users.noreply.github.com>
2024-05-02 14:17:24 +0200

    Actualització main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    basic.showString("" + (input.lightLevel()))
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    led.plotBarGraph(
+    input.lightLevel(),
+    255
+    )
 })
